5cf81bb5821d55626191d2b702a2d87cb46d7e74
phvtquang <quangphamviet0@gmail.com>
2022-03-25 15:06:24 +0700

    add main screen

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coinbaseclone/constant.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -5,8 +6,39 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Container(
+            color: kPrimaryColor,
+            height: MediaQuery.of(context).size.height * 0.4,
+          ),
+          DraggableScrollableSheet(
+              expand: true,
+              initialChildSize: 0.65,
+              minChildSize: 0.65,
+              maxChildSize: 0.85,
+              builder: (BuildContext context, scrollController) {
+                return Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                  ),
+                  child: ListView.builder(
+                    controller: scrollController,
+                    itemCount: 2,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(title: Text('Item $index'));
+                    },
+                  ),
+                );
+              }),
+        ],
+      ),
     );
   }
 }
